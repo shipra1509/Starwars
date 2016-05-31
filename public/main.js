e989f27be5b84c85fe707adf01de9eaf15830e06
Shipra Sinha <shipra.juet@gmail.com>
2016-05-31 12:33:38 +0530

    Handles DELETE request

--- public/main.js
+++ public/main.js
@@ -1,4 +1,5 @@
 var update = document.getElementById('update')
+var del = document.getElementById('delete')
 
 update.addEventListener('click', function () {
   // Sending PUT Request here
@@ -19,6 +20,24 @@ update.addEventListener('click', function () {
   })
 })
 
+del.addEventListener('click', function () {
+  fetch('quotes', {
+    method: 'delete',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      'name': 'Darth Vader'
+    })
+  })
+  .then(res => {
+    if (res.ok) return res.json()
+  }).
+  then(data => {
+    console.log(data)
+    window.location.reload()
+  })
+})
 
 
 
